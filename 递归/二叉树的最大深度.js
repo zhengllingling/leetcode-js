52f30fcb76ef965663328ef5d674cdb60802794b
zhengllingling <1072828530@qq.com>
2021-04-14 11:29:06 +0800

    feat leetcode递归题 二叉树的最大深度解法

--- 递归/二叉树的最大深度.js
+++ 递归/二叉树的最大深度.js
@@ -1,4 +1,8 @@
 
+/** 
+ * 尽量不要使用递归，递归代码虽然相比循环的代码更容易理解， 但是消耗的性能大，容易出错陷入死循环
+ * */
+
 function TreeNode(val, left, right) {
      this.val = (val===undefined ? 0 : val)
      this.left = (left===undefined ? null : left)
