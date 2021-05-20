05eedc59dfe11ffed6fd024e49632a0ab172d8ca
zhengllingling <1072828530@qq.com>
2021-05-20 09:57:27 +0800

    feat 链表js实现修改

--- data-structure/LinkedList.js
+++ data-structure/LinkedList.js
@@ -10,18 +10,13 @@ class LinkedList {
         this.head = null;
         this.size = 0;
     }
-    // get size(){
-    //     return this.size;
-    // }
     find(index) {
-        let orderNumber = 1;
+        let currentIndex = 0;
         let currentNode = this.head;
         while(currentNode) {
-            if(orderNumber === index) {
-                return currentNode
-            }
-            ++orderNumber;
+            if(currentIndex === index) return currentNode;
             currentNode = currentNode.next;
+            currentIndex ++;
         }
         return null;
     }
@@ -38,42 +33,45 @@ class LinkedList {
         this.size++;
     }
     addAtTail(val) {
-        const node = new Node(val, null);
-        let currentNode = this.head;
-        while(currentNode.next) {
-            currentNode = currentNode.next;
+        if(this.size===0) {
+            this.addAtHead(val)
+        }else{
+            const index = this.size - 1;
+            const tailNode = this.find(index);
+            tailNode.next = new Node(val, null);
+            this.size++;
         }
-        currentNode.next = node;
-        this.size++;
     }
     addAtIndex(index, val) {
-        if(index === this.size) {
-            this.addAtTail(val);
-        } else if(index < 0) {
+        if(index <= 0) {
             this.addAtHead(val);
+        } else if(index === this.size) {
+            this.addAtTail(val)
         } else if(index > this.size) {
-            return;
+            return ;
         } else {
-            const prevNode = this.find(index);
-            if(prevNode) {    
-                const node = new Node(val, prevNode.next);
-                prevNode.next = node;
-                this.size ++;
-            }else {
-                this.addAtTail(val);
-            }
+            const prevNode = this.find(index - 1);
+            const node = new Node(val, prevNode.next);
+            prevNode.next = node;
+            this.size++;
         }
     }
     deleteAtIndex(index) {
-        if(index > 0 && index <= this.size) {
-            const preNode = this.find(index - 1);
-            const node = this.find(index);
-            preNode.next = node.next;
+        if(index < 0 || index > (this.size-1)) {
+            return;
+        } else if(index === 0) {
+            const nextNode = this.head.next;
+            this.head.next = null;
+            this.head = nextNode;
             this.size--;
+        } else {
+            const prevNode = this.find(index - 1);
+            const currentNode = prevNode.next;
+            prevNode.next = currentNode.next;
+            currentNode.next = null;
+            this.size--
         }
     }
 }
 
-const list = new LinkedList();
-console.log(list.get(1))
 export default LinkedList;
